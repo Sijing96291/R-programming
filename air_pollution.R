ac6170f50338886a450f17d909256851dee3c41c
Sijing <51327157+Sijing96291@users.noreply.github.com>
2019-06-21 11:13:25 +0800

    week2-part2

--- air_pollution.R
+++ air_pollution.R
@@ -1,3 +1,4 @@
+###############part1###############
 pollutantmean<-function(directory,pollutant,id=1:332){
         #list full list of files in the directory
         files<-list.files(directory,full.names=TRUE)
@@ -12,4 +13,27 @@ pollutantmean<-function(directory,pollutant,id=1:332){
         
         #calculate the mean for the "dat" we just created
         mean(dat[,pollutant],na.rm=TRUE)
+}
+
+
+##################part2########################
+complete <- function(directory,id=1:332){
+        files<-list.files(directory,full.names=TRUE)
+        dat<-data.frame()
+        
+        for(i in id){
+                #number of obs in the selected file that are not missing
+                nobs<-sum(complete.cases(read.csv(files[i])))
+                
+                #create a data frame that contains one obs for each file listed in id
+                tmp<-data.frame(i,nobs)
+                
+                #for each iteration, we add one row to the empty data frame "dat"
+                #from "tmp"
+                dat<-rbind(dat,tmp)
+        }
+        
+        #give the colname for the data frame "dat"
+        colnames(dat)<-c("id","nobs")
+        dat
 }
