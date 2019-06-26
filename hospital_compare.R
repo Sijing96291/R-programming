3e8f79ed22a879613b85a64fb0b65b74cbb2dbad
Sijing <51327157+Sijing96291@users.noreply.github.com>
2019-06-26 15:46:44 +0800

    week4_part3

--- hospital_compare.R
+++ hospital_compare.R
@@ -47,7 +47,92 @@ best<-function(state,outcome){
 
 
 
+#Write a function called rankhospital that returns a character vector with the name of the hospital
+#that has the ranking specified by the num argument.
 
+rankhospital<-function(state,outcome,num="best"){
+        data<-read.csv("outcome-of-care-measures.csv",colClasses = "character")
+        st<-unique(data$State)
+        
+        if (state %in% st){
+                data_sub<-subset(data,data$State==state)
+                
+                if(outcome=="heart attack"){
+                        
+                        #create a new data frame "dat" contains only column 2 and 11
+                        dat<-data_sub[,c(2,11)]
+                        
+                        #rank the data set by heart attack rate in ascending order
+                        #removing all missing values
+                        dat_rank<-dat[order(as.numeric(dat[,2]),dat[,1],na.last=NA),]
+                        
+                        #create a numeric vector "rank" in which contains ranking number
+                        #for each hospital in a given State
+                        rank<-seq(1,nrow(dat_rank),1)
+                        
+                        #create a new column named "Rank"
+                        dat_rank$Rank<-rank
+                        
+                        #select the xth rank from the dataset
+                        if(num>nrow(dat_rank)&&(num!="worst")&&(num!="best")){
+                               print("NA") 
+                        }else if(num=="best"){
+                                print(dat_rank[1,1])
+                        }else if(num=="worst"){
+                                print(dat_rank[nrow(dat_rank),1])
+                        }else{
+                                print(dat_rank[num,1])
+                                #ifelse(num=="best",print(dat_rank[1,1]),
+                                #ifelse(num=="worst",print(dat_rank[nrow(dat_rank),1]),print(dat_rank[num,1])))
+                        }
+                        
+                       
+                        
+                }else if (outcome=="heart failure"){
+                        
+                        dat<-data_sub[,c(2,17)]
+                        dat_rank<-dat[order(as.numeric(dat[,2]),dat[,1],na.last=NA),]
+                        rank<-seq(1,nrow(dat_rank),1)
+                        dat_rank$Rank<-rank
+                        
+                        if(num>nrow(dat_rank)&&(num!="worst")&&(num!="best")){
+                                print("NA") 
+                        }else if(num=="best"){
+                                print(dat_rank[1,1])
+                        }else if(num=="worst"){
+                                print(dat_rank[nrow(dat_rank),1])
+                        }else{
+                                print(dat_rank[num,1])
+                                #ifelse(num=="best",print(dat_rank[1,1]),
+                                #ifelse(num=="worst",print(dat_rank[nrow(dat_rank),1]),print(dat_rank[num,1])))
+                        }
+                        
+                }else if (outcome=="pneumonia"){
+                        
+                        dat<-data_sub[,c(2,23)]
+                        dat_rank<-dat[order(as.numeric(dat[,2]),dat[,1],na.last=NA),]
+                        rank<-seq(1,nrow(dat_rank),1)
+                        dat_rank$Rank<-rank
+                        if(num>nrow(dat_rank)&&(num!="worst")&&(num!="best")){
+                                print("NA") 
+                        }else if(num=="best"){
+                                print(dat_rank[1,1])
+                        }else if(num=="worst"){
+                                print(dat_rank[nrow(dat_rank),1])
+                        }else{
+                                print(dat_rank[num,1])
+                                #ifelse(num=="best",print(dat_rank[1,1]),
+                                #ifelse(num=="worst",print(dat_rank[nrow(dat_rank),1]),print(dat_rank[num,1])))
+                        }
+                        
+                        
+                }else{
+                        stop("invalid outcome")
+                }
+        }else{
+                stop("invalid state")
+                
+        }
+}
 
-
-
+        
